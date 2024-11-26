412def87da2ed85950d008a101a6722f7a2b25d2
AdithamaMulia <130443704+AdithamaMulia@users.noreply.github.com>
2024-11-27 01:42:56 +0700

    renames

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,6 @@
 import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
 
-st.set_page_config(page_title="Home")
+st.set_page_config(page_title="Home", layout="wide", initial_sidebar_state="expanded")
 
 st.title("Optimized Feature Selection for Cancer Detection")
 st.write(
