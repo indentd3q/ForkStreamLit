1253c88f94743f0ad768252490242566018d70e4
AdithamaMulia <130443704+AdithamaMulia@users.noreply.github.com>
2024-11-27 02:07:22 +0700

    finalfinal.

--- pages/4_SVM_Modelling.py
+++ pages/4_SVM_Modelling.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 from sklearn.preprocessing import LabelEncoder
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split, GridSearchCV
