7d6918a9f7693eeaf6ca0ee97b9bd8ab65a6bc17
VincentKurniawan <133878185+indentd3q@users.noreply.github.com>
2024-11-30 00:18:00 +0700

    Sing Rounding

--- pages/3_ROC_Analysis.py
+++ pages/3_ROC_Analysis.py
@@ -35,7 +35,6 @@ if upregulated_file and combined_dataset_file:
         geneID = data.iloc[:,0]
         features_df = data.iloc[:,1:]
         data = data.set_index("Ensembl_ID")
-        data = data.round().astype(int)
         data = data.T
         data['label'] = ['cancer' if '-01' in sample else 'normal' for sample in data.index]
         
